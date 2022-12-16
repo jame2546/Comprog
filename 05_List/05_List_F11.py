bf4b2523d1c1751506756119fecff818c9969abb
Helpmepeet <114396692+Helpmepeet@users.noreply.github.com>
2022-12-16 12:08:19 +0700

    Update 05_List_F11.py

--- 05_List/05_List_F11.py
+++ 05_List/05_List_F11.py
@@ -1,10 +1,10 @@
-def missing_digits(s):
-    numbers = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    for t in s :
-        if t in numbers :
-            numbers.remove(t)
-    if numbers == [] :
-        return "None"
-    else :
-        return [int(e) for e in numbers]
+def missing_digits(t):
+    num = ['0','1','2','3','4','5','6','7','8','9']
+    for e in t : 
+        if e in num :
+            num.remove(e)
+    num = [int(e) for e in num]
+    return num
+        
+
 exec(input()) # DON'T remove this line
